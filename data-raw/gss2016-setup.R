b0f4d585f35db60d86edf38bd9ec83921cbd48a8
wwf2 <wfranko2@gmail.com>
2024-05-01 09:14:19 -0400

    rstudio commit.

--- data-raw/gss2016-setup.R
+++ data-raw/gss2016-setup.R
@@ -1,3 +1,8 @@
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+library(tidyverse)
+library(haven)
+
+gss2016 <- read_dta("data-raw/gss2016.dta")
+
+usethis::use_data(gss2016, overwrite = TRUE)
